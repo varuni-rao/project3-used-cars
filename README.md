4e42d5334a18b5d0537dbeee3fa7ff09a58a0857
Varuni Rao <varuni.rao@gmail.com>
2025-03-24 13:42:57 -0400

    New updated files added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # project3-used-cars
 MLOps final project for MSFT Professional AI program by GL
+
+This is a project where we have to predict the price for used car. The project involves not only building and training a ML model but also using MLOps Pipeline to achieve CI/CD and monitor the performance using mlflow.
--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -15,11 +15,11 @@ import mlflow
 def parse_args():
     '''Parse input arguments'''
 
-    parser = argparse.__________("prep")  # Create an ArgumentParser object
-    parser.add_argument("--raw_data", type=_____, help="Path to raw data")  # Specify the type for raw data (str)
-    parser.add_argument("--train_data", type=_____, help="Path to train dataset")  # Specify the type for train data (str)
-    parser.add_argument("--test_data", type=_____, help="Path to test dataset")  # Specify the type for test data (str)
-    parser.add_argument("--test_train_ratio", type=______, default=_____, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
+    parser = argparse.ArgumentParser("prep")  # Create an ArgumentParser object
+    parser.add_argument("--raw_data", type=str, help="Path to raw data")  # Specify the type for raw data (str)
+    parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train data (str)
+    parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test data (str)
+    parser.add_argument("--test_train_ratio", type=float, default=0.2, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
     args = parser.parse_args()
 
     return args
